01c02fec10d14efe0e82d574694e4889d310fd63
PreeV <preevelury@gmail.com>
2017-02-01 00:49:04 -0500

    Add files via upload

---
+++ SpeakHello.js
@@ -0,0 +1,26 @@
+
+(function (window) {
+var helloSpeaker = {};
+
+var speakWord = "Hello";
+
+helloSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+}
+
+window.helloSpeaker = helloSpeaker;
+
+})(window);
+
+
+
+(function (window) {
+var byeSpeaker = {};
+
+var speakWord = "Good Bye";
+
+byeSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+}
+window.byeSpeaker = byeSpeaker;
+})(window);
